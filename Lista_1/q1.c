6612c8fd03dfaf20a9c8a5c36e6881a0aeb7edfa
Lucca Lima <lucca.kleinsorgen@gmail.com>
2022-04-16 18:33:04 -0300

    Adicionando

---
+++ Lista_1/q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, aux = 0;
+    printf("Valor para a: ");
+    scanf("%d", &a); // entrada valor 1
+    printf("Valor para b: ");
+    scanf("%d", &b); // entrada valor 2
+    aux = a;
+    a = b;
+    b = aux;
+    printf("Valores trocados: a = %d e b = %d.", a, b);
+    // Pausar programa
+    printf("\n");
+    system("pause");
+}
