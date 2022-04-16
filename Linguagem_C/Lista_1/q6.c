3d2269e571cf43b6e3b50d7047f51cf43ff4cdbd
Lucca Lima <lucca.kleinsorgen@gmail.com>
2022-04-16 18:31:49 -0300

    Adicionando

---
+++ Linguagem_C/Lista_1/q6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char codigoConsumidor[10];
+    float total, valorKW, quantidadeKW, valorMin = 21.23;
+
+    printf("Codigo do consumidor (até 10 caracteres): ");
+    scanf("%s", codigoConsumidor);
+    printf("Quantidade consumida: ");
+    scanf("%f", &quantidadeKW);
+    printf("Valor Quilowatt: ");
+    scanf("%f", &valorKW);
+
+    total = valorKW * quantidadeKW;
+    total = (total > valorMin) ? printf("\nO codigo do consumidor: #%s\nTotal a pagar %.2f", codigoConsumidor, total) : printf("\nO codigo do consumidor: #%s\nTotal a pagar %.2f", codigoConsumidor, valorMin);
+    printf("\n");
+    system("pause");
+}
